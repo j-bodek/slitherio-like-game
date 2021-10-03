3459d1327dc58e4fe2499309a4fc84f1d415e810
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-10-03 12:33:06 +0200

    render_food

---
+++ frontend/food.js
@@ -0,0 +1,25 @@
+let canvas_food = document.getElementById('food_box');
+let ctx_food = canvas_food.getContext('2d');
+
+
+let food = function (x, y, radius, color) {
+    ctx_food.beginPath();
+    ctx_food.arc(x, y, radius, 0, Math.PI * 2, true);
+    ctx_food.closePath();
+    ctx_food.fillStyle = color;
+    ctx_food.fill();
+}
+
+
+let display_food = function () {
+    for (let i = 0; i < 200; i++) {
+        let x = Math.floor(Math.random() * 2000);
+        let y = Math.floor(Math.random() * 2000);
+        let radius = (5 + Math.floor(Math.random() * 5));
+        let color = 'red';
+
+        food(x, y, radius, color)
+    }
+}
+
+display_food()
